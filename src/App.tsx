aeddb661e51ae4e817ca57ac945c11bcef8e25e7
newBTD <thanawat.k@borntodev.com>
2023-08-29 12:48:34 +0700

    Add motion for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [motion1, setMotion1] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const handleMotion = () => {
+    window.addEventListener("devicemotion", (e: any) => {
+      setMotion1({
+        x: e.accelerationIncludingGravity.x,
+        y: e.accelerationIncludingGravity.y,
+        z: e.accelerationIncludingGravity.z,
+      });
+    });
+  };
   return (
     <>
-      <h1>Boom!! Chakalaka</h1>
+      <button onClick={handleMotion}></button>
+      <h1>{motion1.x}</h1>
+      <h1>{motion1.y}</h1>
+      <h1>{motion1.z}</h1>
     </>
   );
 }
