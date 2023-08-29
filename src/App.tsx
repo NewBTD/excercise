87709aa9cb1bebbc42c1f7987554ec5a315ff83c
newBTD <thanawat.k@borntodev.com>
2023-08-29 12:54:59 +0700

    Edit Permission for iphone

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { getMobileOperatingSystem } from "../utils/getMobileOperatingSystem";
 import "./App.css";
 
 function App() {
@@ -7,18 +8,41 @@ function App() {
     y: 0,
     z: 0,
   });
-  const handleMotion = () => {
-    window.addEventListener("devicemotion", (e: any) => {
-      setMotion1({
-        x: e.accelerationIncludingGravity.x,
-        y: e.accelerationIncludingGravity.y,
-        z: e.accelerationIncludingGravity.z,
+  const handleRequestMotion = async () => {
+    const mobile = getMobileOperatingSystem();
+    if (mobile === "iOS") {
+      if (typeof (DeviceMotionEvent as any).requestPermission === "function") {
+        (DeviceMotionEvent as any)
+          .requestPermission()
+          .then((permissionState: any) => {
+            if (permissionState === "granted") {
+              window.addEventListener("devicemotion", (e: any) => {
+                setMotion1({
+                  x: e.accelerationIncludingGravity.x,
+                  y: e.accelerationIncludingGravity.y,
+                  z: e.accelerationIncludingGravity.z,
+                });
+              });
+            }
+          })
+          .catch(console.error);
+      } else {
+        // handle regular non iOS 13+ devices
+        console.log("Not Supported");
+      }
+    } else {
+      window.addEventListener("devicemotion", (e: any) => {
+        setMotion1({
+          x: e.accelerationIncludingGravity.x,
+          y: e.accelerationIncludingGravity.y,
+          z: e.accelerationIncludingGravity.z,
+        });
       });
-    });
+    }
   };
   return (
     <>
-      <button onClick={handleMotion}>Enable motion</button>
+      <button onClick={handleRequestMotion}>Enable motion</button>
       <h1>{motion1.x}</h1>
       <h1>{motion1.y}</h1>
       <h1>{motion1.z}</h1>
